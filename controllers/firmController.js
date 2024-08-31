ffa9e1315e3cef0863eab15c5c73515159f60008
siddu2627 <rapolusiddu2627@gmail.com>
2024-08-31 19:04:30 +0530

    added path module

--- controllers/firmController.js
+++ controllers/firmController.js
@@ -1,6 +1,8 @@
 const Firm = require('../models/Firm');
 const Vendor = require('../models/Vendor');
-const multer = require('multer')
+const multer = require('multer');
+const path = require("path");
+
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
